133e0e0c7def6a017a0e23cbb398ed1f07a550a8
jhkwak <zzddtt73@gmail.com>
2024-05-30 21:35:32 +0900

    백준 코딩 테스트 2일차 List, Map, Set 활용이 많았음

--- src/Day2/_10.java
+++ src/Day2/_10.java
@@ -1,7 +1,25 @@
+package Day2;
 import java.util.*;
-
-public class Main {
+public class _10 {
     public static void main(String[] args) {
+        // 문제
+        // 79를 영어로 읽되 숫자 단위로 하나씩 읽는다면 "seven nine"이 된다.
+        // 80은 마찬가지로 "eight zero"라고 읽는다. 79는 80보다 작지만, 영어로 숫자 하나씩 읽는다면 "eight zero"가 "seven nine"보다 사전순으로 먼저 온다.
+        //
+        // 문제는 정수 M, N(1 ≤ M ≤ N ≤ 99)이 주어지면 M 이상 N 이하의 정수를 숫자 하나씩 읽었을 때를 기준으로 사전순으로 정렬하여 출력하는 것이다.
+
+        // 입력
+        // 첫째 줄에 M과 N이 주어진다.
+
+        //출력
+        //M 이상 N 이하의 정수를 문제 조건에 맞게 정렬하여 한 줄에 10개씩 출력한다.
+
+        /**
+         * 1. 반복문을 통해 입력 받은 숫자를 저장한다.
+         * 2. TreeMap (sorting Map) 을 선언하고  numberStringReplace 메서드를 통해 숫자를 문자로 치환 한 값을 key, 기존 숫자를 value 로 저장한다.
+         * 3. 반복문을 통해 value를 출력한다.
+         * 4. 10번째 마다 줄바꿈을 해야되므로 10으로 나눴을때 나머지가 0이면 줄바꿈을 해준다.
+         */
 
         Scanner sc = new Scanner(System.in);
 
